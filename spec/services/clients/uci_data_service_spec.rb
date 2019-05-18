18432c826b24317eeaee7395a2628381c22e2b3e
Blake <blake.enyart@gmail.com>
2019-05-18 17:57:18 -0600

    Add Irises External API request test

--- spec/services/clients/uci_data_service_spec.rb
+++ spec/services/clients/uci_data_service_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 describe 'Irises External API', type: :request do
+  it 'request file and saves in database' do
+    file = File.open('spec/fixtures/data/bezdekIris.data')
+    stub_request(:get, 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/bezdekIris.data')
+      .to_return(status: 200, body: file, headers: {})
 
-  
+    visit '/'
+    click_link 'UCI Iris Dataset'
+
+    expect(current_path).to eq(irises_path)
+    expect(page).to have_content('UCI Iris Dataset')
+    within('.dataset-graphs') do
+      expect(page.have_css('a#graph-1')['src']).to have_content 'assets/images/uci_dataset_graph_1.png'
+    end
+  end
 end
