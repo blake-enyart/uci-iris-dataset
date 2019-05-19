a0300342c451609324a5aa5938fa2580ad4eace3
Blake <blake.enyart@gmail.com>
2019-05-19 14:17:23 -0600

    Add test for Irises internal API

--- spec/requests/api/v1/iris_request_spec.rb
+++ spec/requests/api/v1/iris_request_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
-describe 'Irises Internal API', type: :request do
-  it 'sends a list of irises' do
-    
+describe 'Irises Internal API' do
+  it 'sends a JSON of all irises in db', type: :feature do
+    iris_1, iris_2, iris_3 = create_list(:iris, 3)
     get '/api/v1/irises'
 
     expect(response).to be_successful
 
     parsed = JSON.parse(response.body, symbolize_names: true)[:data]
-
-    expect(parsed.first.keys.include?(:sepal_length)).to eq(true)
-    expect(parsed.first.keys.include?(:sepal_width)).to eq(true)
-    expect(parsed.first.keys.include?(:petal_length)).to eq(true)
-    expect(parsed.first.keys.include?(:petal_width)).to eq(true)
-    expect(parsed.first.keys.include?(:class)).to eq(true)
+    
+    expect(parsed.count).to eq(3)
+    expect(parsed.first[:attributes][:sepal_length]).to eq(iris_1.sepal_length)
+    expect(parsed.first[:attributes][:sepal_width]).to eq(iris_1.sepal_width)
+    expect(parsed.first[:attributes][:petal_length]).to eq(iris_1.petal_length)
+    expect(parsed.first[:attributes][:petal_width]).to eq(iris_1.petal_width)
+    expect(parsed.first[:attributes][:class_type]).to eq(iris_1.class_type)
   end
 end
