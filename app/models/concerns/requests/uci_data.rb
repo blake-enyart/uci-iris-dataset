2a50b530e7290e4d7ddf590d9567ff5b17bbd11f
Blake <blake.enyart@gmail.com>
2019-05-19 13:30:58 -0600

    Rescope so request only connect and receive data

--- app/models/concerns/requests/uci_data.rb
+++ app/models/concerns/requests/uci_data.rb
@@ -3,12 +3,7 @@ module Requests::UciData
     Faraday.new('https://archive.ics.uci.edu/ml/machine-learning-databases/')
   end
 
-  def preprocess_data(response)
-    response.body.split("\n").map { |s| s.split(",")}
-  end
-
   def get_data(url)
     response = conn.get(url)
-    preprocess_data(response)
   end
 end
