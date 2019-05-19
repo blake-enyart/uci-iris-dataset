1feff35848fe903534d7fe147dfeb96be9a183ed
Blake <blake.enyart@gmail.com>
2019-05-19 13:28:56 -0600

    Test for client API call

--- spec/services/clients/uci_data_service_spec.rb
+++ spec/services/clients/uci_data_service_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
-describe 'Irises External API' do
-  it 'request file and saves in database', type: :feature do
+describe Clients::UciDataService do
+  it 'all_irises returns data in appropriate format' do
     file = File.open('spec/fixtures/data/bezdekIris.data')
     stub_request(:get, 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/bezdekIris.data')
       .to_return(status: 200, body: file, headers: {})
 
-    visit '/'
-    click_link 'UCI Iris Dataset'
-    expect(current_path).to eq(irises_path)
+    uci_service = Clients::UciDataService.new
+    all_data = uci_service.all_irises
 
-    expect(page).to have_content('UCI Iris Dataset')
-    within('.dataset-graphs') do
-      expect(page).to have_css('a#graph-1', text: 'Graph 1 Irises')
-      expect(page.find('a#graph-1')['src']).to have_content('assets/images/uci_dataset_graph_1.png')
-    end
+    expect(all_data[0][0]).to eq('5.1')
+    expect(all_data[0][1]).to eq('3.5')
+    expect(all_data[0][2]).to eq('1.4')
+    expect(all_data[0][3]).to eq('0.2')
+    expect(all_data[0][4]).to eq('Iris-setosa')
   end
 end
