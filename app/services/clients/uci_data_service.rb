ba632f0aefc048d520b6764a01d26772bd234ea5
Blake <blake.enyart@gmail.com>
2019-05-18 21:41:38 -0600

    Make query to UCI database and parse date in to database

--- app/services/clients/uci_data_service.rb
+++ app/services/clients/uci_data_service.rb
@@ -13,6 +13,20 @@ class Clients::UciDataService
   end
 
   def all_irises
-    data_raw = Faraday.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/bezdekIris.data')
+    response = Faraday.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/bezdekIris.data')
+    response_lines = response.body.split("\n").map { |s| s.split(",")}
+    Iris.destroy_all
+    response_lines.each do |iris|
+      sepal_length = iris[0].to_f
+      sepal_width = iris[1].to_f
+      petal_length = iris[2].to_f
+      petal_width = iris[3].to_f
+      class_type = iris[4]
+      Iris.create(sepal_length: sepal_length,
+                  sepal_width: sepal_width,
+                  petal_length: petal_length,
+                  petal_width: petal_width,
+                  class_type: class_type)
+    end
   end
 end
