f6b08d23d4f3a11f3c4a25fcaa2d11588fd799a8
Blake <blake.enyart@gmail.com>
2019-05-18 18:11:51 -0600

    Add proxy test of UCI external file consumption

--- spec/services/clients/uci_data_service_spec.rb
+++ spec/services/clients/uci_data_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'Irises External API', type: :request do
-  it 'request file and saves in database' do
+describe 'Irises External API' do
+  it 'request file and saves in database', type: :feature do
     file = File.open('spec/fixtures/data/bezdekIris.data')
     stub_request(:get, 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/bezdekIris.data')
       .to_return(status: 200, body: file, headers: {})
@@ -12,7 +12,8 @@ describe 'Irises External API', type: :request do
     expect(current_path).to eq(irises_path)
     expect(page).to have_content('UCI Iris Dataset')
     within('.dataset-graphs') do
-      expect(page.have_css('a#graph-1')['src']).to have_content 'assets/images/uci_dataset_graph_1.png'
+      expect(page).to have_css('a#graph-1', text: 'Graph 1 Irises')
+      expect(page.find('a#graph-1')['src']).to have_content('assets/images/uci_dataset_graph_1.png')
     end
   end
 end
