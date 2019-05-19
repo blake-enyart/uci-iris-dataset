02dcbe1818eb1bdb6569837da12c58382d55ee62
Blake <blake.enyart@gmail.com>
2019-05-19 09:33:49 -0600

    Add tests for validations of data present

--- spec/models/iris_spec.rb
+++ spec/models/iris_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Iris, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of :sepal_length}
+    it {should validate_presence_of :sepal_width}
+    it {should validate_presence_of :petal_length}
+    it {should validate_presence_of :petal_width}
+    it {should validate_presence_of :class_type}
+  end
 end
