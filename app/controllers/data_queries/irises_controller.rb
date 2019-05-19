4c3bb27200d92c4daba539bc3c9477d143d07d7f
Blake <blake.enyart@gmail.com>
2019-05-19 13:34:06 -0600

    Removes and Creates new Iris data

--- app/controllers/data_queries/irises_controller.rb
+++ app/controllers/data_queries/irises_controller.rb
@@ -1,6 +1,9 @@
 class DataQueries::IrisesController < ApplicationController
-  def index
-    UciFacade.iris_data
-    render_
+  def create
+    Iris.destroy_all
+    UciResultsFacade.new.iris_data.each do |iris|
+      Iris.create!(iris.as_json)
+    end
+    redirect_to irises_path
   end
 end
